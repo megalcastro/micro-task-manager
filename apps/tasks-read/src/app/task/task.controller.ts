5ee270ddefff206f637e3461bed217576aa29cdd
Luis Castro <luisd.ing.pro@gmail.com>
2024-12-31 10:36:20 -0500

    added new services task-read

---
+++ apps/tasks-read/src/app/task/task.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './task.entity';
+
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly tasksService: TaskService) {}
+
+  @Get()
+  getTasks(): Promise<Task[]> {
+    return this.tasksService.getTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+//   @Put(':id')
+//   updateTask(
+//     @Param('id') id: number,
+//     @Body('title') title: string,
+//     @Body('description') description: string,
+//     @Body('status') status: string,
+//     @Body('dueDate') dueDate: Date,
+//   ): Promise<Task> {
+//     return this.tasksService.updateTask(id, title, description, status, dueDate);
+//   }
+
+//   @Delete(':id')
+//   deleteTask(@Param('id') id: number): Promise<void> {
+//     return this.tasksService.deleteTask(id);
+//   }
+}
