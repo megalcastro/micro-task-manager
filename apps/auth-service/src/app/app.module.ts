abda8632937a8a6e12195d0895c3513ed871bed9
Luis Castro <luisd.ing.pro@gmail.com>
2025-01-03 16:30:13 -0500

    fixed auth services

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
     }),
     UsersModule,AuthModule,
     ],
